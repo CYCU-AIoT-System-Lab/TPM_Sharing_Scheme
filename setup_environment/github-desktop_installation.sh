a05a906bc065f41601ec5853dc9b8a6b674da6bb
belongtothenight <106849935+belongtothenight@users.noreply.github.com>
2023-10-12 14:49:58 +0800

    Update github-desktop_installation.sh

--- setup_environment/github-desktop_installation.sh
+++ setup_environment/github-desktop_installation.sh
@@ -44,3 +44,6 @@ sudo gdebi GitHubDesktop-linux-3.3.3-linux1.deb
 ## Initial post
 #sudo wget https://github.com/shiftkey/desktop/releases/download/release-2.1.0-linux1/GitHubDesktop-linux-2.1.0-linux1.deb
 #sudo gdebi GitHubDesktop-linux-2.1.0-linux1.deb
+
+# Launch Github Desktop
+github .
