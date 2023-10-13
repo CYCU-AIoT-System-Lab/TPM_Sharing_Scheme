a06645b447cfea371256d87848b2553dcc4ebe6e
belongtothenight <106849935+belongtothenight@users.noreply.github.com>
2023-10-13 16:16:03 +0800

    Adjust script output message for readability

--- setup_environment/installation/swtpm-simulator_installation.sh
+++ setup_environment/installation/swtpm-simulator_installation.sh
@@ -18,11 +18,14 @@
 # =====================================================================================================
 #!/bin/bash
 
+echo -e "\n=====================================================================================================\nStart installing swtpm simulator ...\n=====================================================================================================\n"
+
 # acquire the current directory
 path=$(pwd)
 
 # install dependencies for libtpms
 apt -y install dpkg-dev debhelper libssl-dev libtool net-tools libfuse-dev libglib2.0-dev libgmp-dev expect libtasn1-dev socat python3-twisted gnutls-dev gnutls-bin  libjson-glib-dev gawk git python3-setuptools softhsm2 libseccomp-dev automake autoconf libtool gcc build-essential libssl-dev dh-exec pkg-config dh-autoreconf libtool-bin tpm2-tools libtss0 libtss2-dev dh-apparmor swtpm-tools devscripts equivs
+# if can't get swtpm-tools, try to retart the entire process.
 
 # install libtpms
 git clone https://github.com/stefanberger/libtpms.git
