3891f33262dc6e9eba80befb828267b362c7105a
belongtothenight <106849935+belongtothenight@users.noreply.github.com>
2023-10-14 11:00:05 +0800

    add daemen killer

---
+++ setup_environment/operation/tpm_daemen_killer.sh
@@ -0,0 +1,30 @@
+# =====================================================================================================
+# The following code is modified from the following source:
+# 1. https://francislampayan.medium.com/how-to-setup-tpm-simulator-in-ubuntu-20-04-25ec673b88dc
+# 2. https://gist.github.com/fbdlampayan/5ceaadda9c32d4c23478ee46f80207f9#file-gistfile1-txt
+# =====================================================================================================
+# Related Source:
+# =====================================================================================================
+# Modified by: Dachuan Chen
+# Date: 2023/10/14
+# Removed all "sudo" commands.
+# Nevigate to your desired directory and run this script with "sudo ./tpm_daemen_killer.sh"
+# =====================================================================================================
+# Issue:
+# =====================================================================================================
+#!/bin/bash
+
+echo -e "\n=====================================================================================================\nStart installing tpm_daemen_killer ...\n=====================================================================================================\n"
+
+# Manipulate services
+# 1. tpm2-abrmd.service
+# 2. tpm-server.service
+
+# Stop tpm2-abrmd.service
+systemctl stop tpm2-abrmd.service
+
+# Stop tpm-server.service
+systemctl stop tpm-server.service
+
+# Message
+echo -e "\n=====================================================================================================\nAll TPM daemons are killed.\n=====================================================================================================\n"
--- setup_environment/readme.md
+++ setup_environment/readme.md
@@ -12,7 +12,23 @@ Testing environment: Fresh installed ubuntu-20.04.6-desktop-amd64.iso in VM.
 4. ```cp ./TPM_Sharing_Scheme-setup_environment/setup_environment/installation/*.sh ./```
 5. ```sudo chmod +x setup_environment.sh```
 6. ```sudo ./setup_environment.sh```
+7. Reboot system with ```sudo reboot```
 
-## Debug list
+## Usage
+
+1. Use SW TPM (For TPM simulator development)
+   1. Activate
+      1. Open terminal, ```./setup_environment/activate/activate_swtpm_simulator.sh```.
+      2. Open new terminal, ```./setup_environment/operation/tpm2tss_swtpm_start.sh``` to start SW TPM.
+   2. Reset
+      1. ```./setup_environment/operation/tpm2tss_swtpm_reset.sh```
+
+## Debug List
 
 1. "tpm2tss.so" location.
+
+## Feature List
+
+1. TPM daemen killer.
+2. TPM daemen starter.
+3. TPM simulator & pysical chip daemen switcher.
