bdc532837d3360e65e1cd8919398a190f3ab0d18
belongtothenight <106849935+belongtothenight@users.noreply.github.com>
2023-11-03 19:52:57 +0800

    Update setup.sh

--- setup_ibmtpm/setup.sh
+++ setup_ibmtpm/setup.sh
@@ -1,16 +1,19 @@
-#!/bin/bash -x
+#!/bin/bash
 
 download_dir="/home/user/Downloads"
 
 echo -e "\n====================================================\nSetup IBMTPM\n====================================================\n"
 
+echo -e "Copying files to ${download_dir}..."
 cp ./*.sh "${download_dir}"
 
+echo -e "Changing permissions..."
 chmod +x "${download_dir}/install_req.sh"
 chmod +x "${download_dir}/install_ibmswtpm.sh"
 chmod +x "${download_dir}/install_ibmtpm2tss.sh"
 chmod +x "${download_dir}/install_ibmacs.sh"
 
+echo -e "Installing IBMTPM..."
 bash "${download_dir}/install_req.sh"
 bash "${download_dir}/install_ibmswtpm.sh"
 bash "${download_dir}/install_ibmtpm2tss.sh"
