b97627e9fe028ca079a60076f16e4f6ecfb8f945
belongtothenight <106849935+belongtothenight@users.noreply.github.com>
2023-11-03 20:13:57 +0800

    Update install_req.sh

--- setup_ibmtpm/install_req.sh
+++ setup_ibmtpm/install_req.sh
@@ -7,17 +7,22 @@ ibmacs_ver="1658"
 
 echo -e "\n====================================================\nInstalling IBMTPM\n====================================================\n"
 
-echo -e "Updating system..."
+BLUE='\033[0;34m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+echo -e "${BLUE}Updating system...${NC}"
 apt-get update
 apt-get upgrade -y
 
-echo -e "Installing tools..."
+echo -e "${BLUE}Installing tools...${NC}"
 apt-get install -y htop iftop neovim git curl wget
 
-echo -e "Installing IBMTPM dependencies..."
+echo -e "${BLUE}Installing IBMTPM dependencies...${NC}"
 apt-get install -y build-essential make gcc libssl-dev
 
-echo -e "Downloading IBMTPM..."
+echo -e "${BLUE}Downloading IBMTPM...${NC}"
 file_ibmtss="${download_dir}/ibmtss${ibmtss_ver}.tar.gz"
 file_ibmtpm="${download_dir}/ibmtpm${ibmtpm_ver}.tar.gz"
 file_ibmacs="${download_dir}/ibmacs${ibmacs_ver}.tar.gz"
@@ -25,7 +30,7 @@ curl -L https://sourceforge.net/project/ibmtpm20tss/files/ibmtss${ibmtss_ver}.ta
 curl -L https://sourceforge.net/project/ibmswtpm2/files/ibmtpm${ibmtpm_ver}.tar.gz > ${file_ibmtpm}
 curl -L https://sourceforge.net/projects/ibmtpm20acs/files/ibmacs${ibmacs_ver}.tar.gz > ${file_ibmacs}
 
-echo -e "Creating directories..."
+echo -e "${BLUE}Creating directories...${NC}"
 path_ibmtss="/opt/ibmtss${ibmtss_ver}/"
 path_ibmtpm="/opt/ibmtpm${ibmtpm_ver}/"
 path_ibmacs="/opt/ibmacs${ibmacs_ver}/"
@@ -33,12 +38,12 @@ mkdir "${path_ibmtss}"
 mkdir "${path_ibmtpm}"
 mkdir "${path_ibmacs}"
 
-echo -e "Copying files to /opt..."
+echo -e "${BLUE}Copying files to /opt...${NC}"
 cp ${file_ibmtss} ${path_ibmtss}
 cp ${file_ibmtpm} ${path_ibmtpm}
 cp ${file_ibmacs} ${path_ibmacs}
 
-echo -e "Extracting files..."
+echo -e "${BLUE}Extracting files...${NC}"
 tar -zxvf "/opt/ibmtss${ibmtss_ver}/ibmtss${ibmtss_ver}.tar.gz"
 tar -zxvf "/opt/ibmtpm${ibmtpm_ver}/ibmtpm${ibmtpm_ver}.tar.gz"
 tar -zxvf "/opt/ibmacs${ibmacs_ver}/ibmacs${ibmacs_ver}.tar.gz"
