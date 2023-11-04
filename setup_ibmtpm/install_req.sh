8abb88f7d175fa35ce81b03b88404c87e3bce1b7
belongtothenight <106849935+belongtothenight@users.noreply.github.com>
2023-11-04 13:29:06 +0800

    Improve CLI effect

--- setup_ibmtpm/install_req.sh
+++ setup_ibmtpm/install_req.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
+# ==================================================================================================
+# Parameters
 download_dir="/home/user/Downloads"
 ibmtss_ver="2.1.1"
 ibmtpm_ver="1682"
 ibmacs_ver="1658"
-
-echo -e "\n====================================================\nInstalling IBMTPM\n====================================================\n"
+# ==================================================================================================
 
 BLUE='\033[0;34m'
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m'
+
+echo -e "\n====================================================\n${GREEN}Installing IBMTPM${NC}\n====================================================\n"
+
 fn_ibmtss="ibmtss${ibmtss_ver}.tar.gz"
 fn_ibmtpm="ibmtpm${ibmtpm_ver}.tar.gz"
 fn_ibmacs="ibmacs${ibmacs_ver}.tar.gz"
@@ -21,32 +25,32 @@ path_ibmtss="/opt/ibmtss${ibmtss_ver}/"
 path_ibmtpm="/opt/ibmtpm${ibmtpm_ver}/"
 path_ibmacs="/opt/ibmacs${ibmacs_ver}/"
 
-echo -e "${BLUE}Updating system...${NC}"
+echo -e "\n${BLUE}Updating system...${NC}"
 apt-get update
 apt-get upgrade -y
 
-echo -e "${BLUE}Installing tools...${NC}"
+echo -e "\n${BLUE}Installing tools...${NC}"
 apt-get install -y htop iftop neovim git curl wget
 
-echo -e "${BLUE}Installing IBMTPM dependencies...${NC}"
+echo -e "\n${BLUE}Installing IBMTPM dependencies...${NC}"
 apt-get install -y build-essential make gcc libssl-dev
 
-echo -e "${BLUE}Downloading IBMTPM...${NC}"
+echo -e "\n${BLUE}Downloading IBMTPM...${NC}"
 wget "https://sourceforge.net/projects/ibmtpm20tss/files/${fn_ibmtss}/download" -O ${file_ibmtss}
 wget "https://sourceforge.net/projects/ibmswtpm2/files/${fn_ibmtpm}/download" -O ${file_ibmtpm}
 wget "https://sourceforge.net/projects/ibmtpm20acs/files/${fn_ibmacs}/download" -O ${file_ibmacs}
 
-echo -e "${BLUE}Creating directories...${NC}"
+echo -e "\n${BLUE}Creating directories...${NC}"
 mkdir "${path_ibmtss}"
 mkdir "${path_ibmtpm}"
 mkdir "${path_ibmacs}"
 
-echo -e "${BLUE}Copying files to /opt...${NC}"
+echo -e "\n${BLUE}Copying files to /opt...${NC}"
 cp ${file_ibmtss} ${path_ibmtss}
 cp ${file_ibmtpm} ${path_ibmtpm}
 cp ${file_ibmacs} ${path_ibmacs}
 
-echo -e "${BLUE}Extracting files...${NC}"
+echo -e "\n${BLUE}Extracting files...${NC}"
 tar -zxvf "${path_ibmtss}/${fn_ibmtss}" -C ${path_ibmtss}
 tar -zxvf "${path_ibmtpm}/${fn_ibmtpm}" -C ${path_ibmtpm}
 tar -zxvf "${path_ibmacs}/${fn_ibmacs}" -C ${path_ibmacs}
--- setup_ibmtpm/setup.sh
+++ setup_ibmtpm/setup.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 
+# ==================================================================================================
+# Parameters
 download_dir="/home/user/Downloads"
+# ==================================================================================================
 
-echo -e "\n====================================================\nSetup IBMTPM\n====================================================\n"
+echo -e "\n====================================================\n${GREEN}ISetup IBMTPM${NC}\n====================================================\n"
 
-echo -e "Copying files to ${download_dir}..."
+echo -e "\n${BLUE}Copying files to ${download_dir}...${NC}"
 cp ./*.sh "${download_dir}"
 
-echo -e "Changing permissions..."
+echo -e "\n${BLUE}Changing permissions...${NC}"
 chmod +x "${download_dir}/install_req.sh"
 chmod +x "${download_dir}/install_ibmswtpm.sh"
 chmod +x "${download_dir}/install_ibmtpm2tss.sh"
 chmod +x "${download_dir}/install_ibmacs.sh"
 
-echo -e "Installing IBMTPM..."
-bash +x "${download_dir}/install_req.sh"
-bash +x "${download_dir}/install_ibmswtpm.sh"
-bash +x "${download_dir}/install_ibmtpm2tss.sh"
-bash +x "${download_dir}/install_ibmacs.sh"
+echo -e "\n${BLUE}Installing IBMTPM...${NC}"
+bash "${download_dir}/install_req.sh"
+bash "${download_dir}/install_ibmswtpm.sh"
+bash "${download_dir}/install_ibmtpm2tss.sh"
+bash "${download_dir}/install_ibmacs.sh"
 
-echo -e "\n====================================================\nSetup IBMTPM Complete\n====================================================\n"
+echo -e "\n====================================================\n${GREEN}Setup IBMTPM Complete${NC}\n====================================================\n"
