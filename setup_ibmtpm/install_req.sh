6cc5ab7892f43623ed0eac3a35c0bb686eadac79
belongtothenight <106849935+belongtothenight@users.noreply.github.com>
2023-11-04 12:50:26 +0800

    Update install_req.sh

--- setup_ibmtpm/install_req.sh
+++ setup_ibmtpm/install_req.sh
@@ -11,6 +11,15 @@ BLUE='\033[0;34m'
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m'
+fn_ibmtss="ibmtss${ibmtss_ver}.tar.gz"
+fn_ibmtpm="ibmtpm${ibmtpm_ver}.tar.gz"
+fn_ibmacs="ibmacs${ibmacs_ver}.tar.gz"
+file_ibmtss="${download_dir}/${fn_ibmtss}"
+file_ibmtpm="${download_dir}/${fn_ibmtpm}"
+file_ibmacs="${download_dir}/${fn_ibmacs}"
+path_ibmtss="/opt/ibmtss${ibmtss_ver}/"
+path_ibmtpm="/opt/ibmtpm${ibmtpm_ver}/"
+path_ibmacs="/opt/ibmacs${ibmacs_ver}/"
 
 echo -e "${BLUE}Updating system...${NC}"
 apt-get update
@@ -23,17 +32,11 @@ echo -e "${BLUE}Installing IBMTPM dependencies...${NC}"
 apt-get install -y build-essential make gcc libssl-dev
 
 echo -e "${BLUE}Downloading IBMTPM...${NC}"
-file_ibmtss="${download_dir}/ibmtss${ibmtss_ver}.tar.gz"
-file_ibmtpm="${download_dir}/ibmtpm${ibmtpm_ver}.tar.gz"
-file_ibmacs="${download_dir}/ibmacs${ibmacs_ver}.tar.gz"
-wget "https://sourceforge.net/projects/ibmswtpm2/files/ibmtpm${ibmtpm_ver}.tar.gz/download" -O ${file_ibmtpm}
-wget "https://sourceforge.net/projects/ibmswtpm2/files/ibmtss${ibmtss_ver}.tar.gz/download" -O ${file_ibmtss}
-wget "https://sourceforge.net/projects/ibmtpm20acs/files/ibmacs${ibmacs_ver}.tar.gz/download" -O ${file_ibmacs}
+wget "https://sourceforge.net/projects/ibmtpm20tss/files/${fn_ibmtss}/download" -O ${file_ibmtss}
+wget "https://sourceforge.net/projects/ibmswtpm2/files/${fn_ibmtpm}/download" -O ${file_ibmtpm}
+wget "https://sourceforge.net/projects/ibmtpm20acs/files/${fn_ibmacs}/download" -O ${file_ibmacs}
 
 echo -e "${BLUE}Creating directories...${NC}"
-path_ibmtss="/opt/ibmtss${ibmtss_ver}/"
-path_ibmtpm="/opt/ibmtpm${ibmtpm_ver}/"
-path_ibmacs="/opt/ibmacs${ibmacs_ver}/"
 mkdir "${path_ibmtss}"
 mkdir "${path_ibmtpm}"
 mkdir "${path_ibmacs}"
@@ -44,8 +47,8 @@ cp ${file_ibmtpm} ${path_ibmtpm}
 cp ${file_ibmacs} ${path_ibmacs}
 
 echo -e "${BLUE}Extracting files...${NC}"
-tar -zxvf "/opt/ibmtss${ibmtss_ver}/ibmtss${ibmtss_ver}.tar.gz" -C ${path_ibmtss}
-tar -zxvf "/opt/ibmtpm${ibmtpm_ver}/ibmtpm${ibmtpm_ver}.tar.gz" -C ${path_ibmtpm}
-tar -zxvf "/opt/ibmacs${ibmacs_ver}/ibmacs${ibmacs_ver}.tar.gz" -C ${path_ibmacs}
+tar -zxvf "${path_ibmtss}/${fn_ibmtss}" -C ${path_ibmtss}
+tar -zxvf "${path_ibmtpm}/${fn_ibmtpm}" -C ${path_ibmtpm}
+tar -zxvf "${path_ibmacs}/${fn_ibmacs}" -C ${path_ibmacs}
 
 echo -e "\n====================================================\nInstalling IBMTPM Complete\n====================================================\n"
