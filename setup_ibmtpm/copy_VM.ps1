49a91e5db6ca930e7a76b367ef33b7f85b410f4e
belongtothenight <106849935+belongtothenight@users.noreply.github.com>
2023-11-06 16:09:20 +0800

    Update copy_VM.ps1

--- setup_ibmtpm/copy_VM.ps1
+++ setup_ibmtpm/copy_VM.ps1
@@ -4,13 +4,14 @@ $srcVM2 = "Ubuntu 64-bit 18.04 Desktop 0x13 Bak"
 $dstVM1 = "Ubuntu 64-bit 18.04 Desktop 0x15"
 $dstVM2 = "Ubuntu 64-bit 18.04 Desktop 0x16"
 
-$cp_command = "cp -R -force"
+# $cp_command = "cp -R -force"
+$cp_command = "cmd /c copy /z"
 $jobs = "$cp_command '$baseDir\$srcVM1' '$baseDir\$dstVM1'", 
-        "$cp_command '$baseDir\$srcVM1' '$baseDir\$dstVM2'",
+        "$cp_command '$baseDir\$srcVM1' '$baseDir\$dstVM2'"
 
 echo "Copying VMs, check disk usage in task manager."
 foreach($job in $jobs) {
-    echo "Starting job: $job"
+    echo "Running: $job"
     Invoke-Expression $job
 }
 echo "Done."
