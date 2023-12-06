e716da48b7859ca877548188d9131a398ff7830c
belongtothenight <106849935+belongtothenight@users.noreply.github.com>
2023-12-06 20:08:08 +0800

    Update copy_VM.ps1

--- setup_ibmtpm/copy_VM.ps1
+++ setup_ibmtpm/copy_VM.ps1
@@ -1,8 +1,8 @@
 $baseDir = "E:/Virtual Machines"
 $srcVM1 = "Ubuntu 64-bit 18.04 Desktop 0x12 Bak"
 $srcVM2 = "Ubuntu 64-bit 18.04 Desktop 0x13 Bak"
-$dstVM1 = "Ubuntu 64-bit 18.04 Desktop 0x17"
-$dstVM2 = "Ubuntu 64-bit 18.04 Desktop 0x18"
+$dstVM1 = "Ubuntu 64-bit 18.04 Desktop 0x19"
+$dstVM2 = "Ubuntu 64-bit 18.04 Desktop 0x20"
 
 # $cp_command = "cp -R -force"
 #$cp_command = "cmd /c xcopy /h /i /c /k /e /r /y"
