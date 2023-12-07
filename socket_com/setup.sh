ec65421db9bbc9f08e75341fe05cbb6c6e3b2f49
belongtothenight <106849935+belongtothenight@users.noreply.github.com>
2023-12-07 22:59:43 +0800

    Update setup.sh

--- socket_com/setup.sh
+++ socket_com/setup.sh
@@ -176,7 +176,7 @@ fi
 if [ $host_docs -eq 1 ]; then
 	echo -e "${term_notice_setup}Hosting documentation in new terminal..."
 	cd "${doc_dir}/html"
-	launch_cmd="echo -e \"${term_notice_docs}Hosting documentation...\"; python3 -m http.server --bind ${host_ip} --directory ${doc_dir}/html ${host_port}"
+	launch_cmd="echo -e \"${term_notice_docs}Hosting documentation...\"; python3 -m http.server --bind ${host_ip} ${host_port}"
 	gnome-terminal -- bash -c "${launch_cmd}; exec bash"
 elif [ $host_docs -eq 0 ]; then
 	echo -e "${term_notice_setup}Skipped hosting documentation!"
