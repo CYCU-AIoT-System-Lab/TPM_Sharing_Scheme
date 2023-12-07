40d9ed1ddedf296aa6e3094ad305bbd2e4affe7a
belongtothenight <106849935+belongtothenight@users.noreply.github.com>
2023-12-07 11:20:25 +0800

    Update setup.sh

--- server_com/setup.sh
+++ server_com/setup.sh
@@ -1,28 +1,35 @@
 #!/bin/bash
 
 # Setup Tasks (0=No, 1=Yes)
-install_client=1
-install_server=1
-
-# SubTasks-dev (0=No, 1=Yes)
+run_client=1
+run_server=1
 
 # General Settings
 proj_dir="${PWD}"
 term_notice="\033[1m\033[34m[NOTICE]\033[0m "
 term_warn="\033[1m\033[33m[WARNING]\033[0m "
 
+# SubTasks-dev (0=No, 1=Yes)
+perform_clean=1
+perform_build=1
+
+# Do not adjust below this line
+# -----------------------------
+compile_client=$((perform_build * run_client))
+compile_server=$((perform_build * run_server))
+
 # Delete cmake subproject
 awk '!/add_subdirectory/' "${proj_dir}/CMakeLists.txt" > "${proj_dir}/tmp.txt"
 mv "${proj_dir}/tmp.txt" "${proj_dir}/CMakeLists.txt"
 
 # Add client subproject
-if [ $install_client -eq 1 ]; then
+if [ $compile_client -eq 1 ]; then
 	echo -e "${term_notice}Adding client compiling task..."
 	echo "add_subdirectory(\${CMAKE_SOURCE_DIR}/client)" >> "${proj_dir}/CMakeLists.txt"
 fi
 
 # Add server subproject
-if [ $install_server -eq 1 ]; then
+if [ $compile_server -eq 1 ]; then
 	echo -e "${term_notice}Adding server compiling task..."
 	echo "add_subdirectory(\${CMAKE_SOURCE_DIR}/server)" >> "${proj_dir}/CMakeLists.txt"
 fi
