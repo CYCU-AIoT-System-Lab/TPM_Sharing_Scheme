07d3ecf9f0bd9824557e01111529d5b1300797a9
belongtothenight <106849935+belongtothenight@users.noreply.github.com>
2023-12-07 19:30:53 +0800

    Update setup.sh

--- socket_com/setup.sh
+++ socket_com/setup.sh
@@ -126,11 +126,11 @@ fi
 # Run Client
 cd "${proj_dir}/bin"
 
-gnome-terminal -- bash -c "sleep 5; exit; exec bash"
+xdotool search --onlyvisible --limit 1 --name "@" set_window --name "setup.sh"
 
 if [ $run_client -eq 1 ]; then
 	echo -e "${term_notice}Running client on new terminal..."
-	launch_cmd="xdotool getactivewindow set_window --name \"socket_com client\"; echo -e \"${term_notice}Starting client...\"; ./client"
+	launch_cmd="xdotool search --onlyvisible --limit 1 --name \"@\" set_window --name \"socket_com client\"; echo -e \"${term_notice}Starting client...\"; ./client"
 	gnome-terminal -- bash -c "${launch_cmd}; exec bash"
 elif [ $run_client -eq 0 ]; then
 	echo -e "${term_notice}Skipped running client!"
