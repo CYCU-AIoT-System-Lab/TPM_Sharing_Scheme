4e36268b820afbf8f6416c65486164ce1a78ce80
belongtothenight <106849935+belongtothenight@users.noreply.github.com>
2023-12-07 10:11:40 +0800

    Update setup.sh

--- server_com/setup.sh
+++ server_com/setup.sh
@@ -4,9 +4,29 @@
 install_client=1
 install_server=1
 
-# Set working directory
+# SubTasks-dev (0=No, 1=Yes)
+
+# General Settings
 proj_dir="${PWD}"
+term_notice="\033[1m\033[34m[NOTICE]\033[0m]"
+term_warn="\033[1m\033[33m[WARNING]\033[0m]"
 
 # Delete cmake subproject
 awk '!/add_subdirectory/' "${proj_dir}/CMakeLists.txt" > "${proj_dir}/tmp.txt"
 mv "${proj_dir}/tmp.txt" "${proj_dir}/CMakeLists.txt"
+
+# Add client subproject
+if [ $install_client -eq 1 ]; then
+	echo -e "${term_notice}Adding client subproject..."
+	echo "add_subdirectory(${CMAKE_SOURCE_DIR}/client)" >> "${proj_dir}/CMakeLists.txt"
+fi
+
+# Add server subproject
+if [ $install_server -eq 1 ]; then
+	echo -e "${term_notice}Adding server subproject..."
+	echo "add_subdirectory(${CMAKE_SOURCE_DIR}/server)" >> "${proj_dir}/CMakeLists.txt"
+fi
+
+
+# Finish
+echo -e "${term_notice}Setup complete."
