3c5e60f82df85f2ea9fa9fd4004a1f217f09c2e8
belongtothenight <106849935+belongtothenight@users.noreply.github.com>
2023-12-10 22:34:35 +0800

    Update setup.sh

--- common/setup.sh
+++ common/setup.sh
@@ -14,10 +14,10 @@ setup_socket_com=1
 
 install_req () {
 	aptins () {
-		echo -e "$term_notice Installing $1..."
+		echo -e "${term_notice}Installing $1..."
 		sudo apt-get install -y $1
 	}
-	echo -e "$term_notice Installing required packages..."
+	echo -e "${term_notice} Installing required packages..."
 	sudo apt-get update
 	sudo apt-get upgrade -y
 	aptins "git"
@@ -34,25 +34,25 @@ install_req () {
 }
 
 config_nvim () {
-	echo -e "$term_notice Configuring neovim..."
+	echo -e "${term_notice}Configuring neovim..."
 	mkdir -p $nvim_dir
 	wget "$nvim_config_url" -O "${nvim_dir}/init.vim"
 }
 
 change_all_sh_mod () {
-	echo -e "$term_notice Changing all .sh files to executable..."
+	echo -e "${term_notice}Changing all .sh files to executable..."
 	find .. -type f -iname "*.sh" -exec sudo chmod +x {} \;
 }
 
 update_src () {
-	echo -e "$term_notice Pulling latest repo source..."
+	echo -e "${term_notice}Pulling latest repo source..."
 	git stash
 	git stash clear
 	git pull
 }
 
 config_apport () {
-	echo -e "$term_notice Configuring apport..."
+	echo -e "${term_notice}Configuring apport..."
 	ulimit -c unlimited
 	mkdir -p $apport_dir
 	touch $apport_dir/settings
@@ -63,39 +63,39 @@ config_apport () {
 }
 
 reload_term () {
-	echo -e "$term_notice Reloading terminal..."
+	echo -e "${term_notice}Reloading terminal..."
 	source ~/.bashrc
 }
 
-echo -e "$term_notice Running common setup..."
-echo -e "$term_notice Current directory: $PWD"
+echo -e "${term_notice}Running common setup..."
+echo -e "${term_notice}Current directory: $PWD"
 install_req
 config_nvim
 update_src
 change_all_sh_mod
 config_apport
 reload_term
-echo -e "$term_notice Common setup complete."
+echo -e "${term_notice}Common setup complete."
 
 if [ $setup_environment -eq 1 ]; then
-	echo -e "$term_warn Running environment setup Not Implemneted Yet!"
+	echo -e "${term_warn}Running environment setup Not Implemneted Yet!"
 else
-	echo -e "$term_warn Invalid Argument: $setup_environment ! Skipping setup_environment..."
+	echo -e "${term_warn}Invalid Argument: $setup_environment ! Skipping setup_environment..."
 fi
 
 if [ $setup_ibmtpm -eq 1 ]; then
-	echo -e "$term_warn Running ibmtpm setup Not Implemneted Yet!"
+	echo -e "${term_warn}Running ibmtpm setup Not Implemneted Yet!"
 else
-	echo -e "$term_warn Invalid Argument: $setup_ibmtpm ! Skipping setup_ibmtpm..."
+	echo -e "${term_warn}Invalid Argument: $setup_ibmtpm ! Skipping setup_ibmtpm..."
 fi
 
 if [ $setup_socket_com -eq 1 ]; then
-	echo -e "$term_notice Running socket_com setup..."
+	echo -e "${term_notice}Running socket_com setup..."
 	cd ../socket_com
 	./setup.sh
 	cd ..
 else
-	echo -e "$term_warn Invalid Argument: $setup_socket_com ! Skipping setup_socket_com..."
+	echo -e "${term_warn}Invalid Argument: $setup_socket_com ! Skipping setup_socket_com..."
 fi
 
-echo -e "$term_notice All setup complete."
+echo -e "${term_notice}All setup complete."
