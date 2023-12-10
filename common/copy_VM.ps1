7b54986d98bfeb31936dd059e68dd23be2ab07a5
belongtothenight <106849935+belongtothenight@users.noreply.github.com>
2023-12-10 23:48:10 +0800

    Update copy_VM.ps1

--- common/copy_VM.ps1
+++ common/copy_VM.ps1
@@ -1,14 +1,18 @@
 $baseDir = "E:/Virtual Machines"
 $srcVM1 = "Ubuntu 64-bit 18.04 Desktop 0x12 Bak"
 $srcVM2 = "Ubuntu 64-bit 18.04 Desktop 0x13 Bak"
-$dstVM1 = "Ubuntu 64-bit 18.04 Desktop 0x1a"
-$dstVM2 = "Ubuntu 64-bit 18.04 Desktop 0x1b"
+$dstVM1 = "Ubuntu 64-bit 18.04 Desktop 0x1c"
+$dstVM2 = "Ubuntu 64-bit 18.04 Desktop 0x1d"
+$dstVM3 = "Ubuntu 64-bit 18.04 Desktop 0x1e"
+$dstVM4 = "Ubuntu 64-bit 18.04 Desktop 0x1f"
 
 # $cp_command = "cp -R -force"
 #$cp_command = "cmd /c xcopy /h /i /c /k /e /r /y"
 $cp_command = "robocopy -E /ETA"
 $jobs = "$cp_command `"$baseDir/$srcVM1`" `"$baseDir/$dstVM1`"", 
-        "$cp_command `"$baseDir/$srcVM1`" `"$baseDir/$dstVM2`""
+        "$cp_command `"$baseDir/$srcVM1`" `"$baseDir/$dstVM2`"",
+		"$cp_command `"$baseDir/$srcVM2`" `"$baseDir/$dstVM3`"",
+		"$cp_command `"$baseDir/$srcVM2`" `"$baseDir/$dstVM4`""
 
 echo "Copying VMs, check disk usage in task manager."
 foreach($job in $jobs) {
