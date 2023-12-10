9f9b009c817dea3dda5cd685ae81c88517687ca2
belongtothenight <106849935+belongtothenight@users.noreply.github.com>
2023-12-10 22:43:39 +0800

    Update setup.sh

--- socket_com/setup.sh
+++ socket_com/setup.sh
@@ -121,9 +121,9 @@ fi
 # Build
 if [ $build_for_debug -eq 1 ]; then
 	echo -e "${term_notice_setup}Generating makefile for debug..."
-	cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=.. -DBUILD_SHARED_LIBS=ON -- VERBOSE=1
+	cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=.. -DBUILD_SHARED_LIBS=ON
 	echo -e "${term_notice_setup}Building and installing project..."
-	make -j"$(nproc)" install
+	make -j"$(nproc)" install VERBOSE=1
 else
 	echo -e "${term_notice_setup}Generating makefile for release..."
 	cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=.. -DBUILD_SHARED_LIBS=ON
