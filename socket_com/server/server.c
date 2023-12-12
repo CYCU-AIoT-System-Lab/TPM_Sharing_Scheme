9b098b2c7a386582f0349b781d1495461151427f
belongtothenight <106849935+belongtothenight@users.noreply.github.com>
2023-12-12 19:22:03 +0800

    try using lib

--- socket_com/server/server.c
+++ socket_com/server/server.c
@@ -4,6 +4,7 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
+#include "../lib/output_format.h"
 
 /**
  * @brief Main function for the server part of project
@@ -14,5 +15,7 @@
 int main(int argc, char *argv[])
 {
 	printf("Hello from server!\n");
+	output_format_t *pFormat = init_output_format("SERVER");
+	printf("%sHello from server!\n", pFormat.info);;
 	return 0;
 }
