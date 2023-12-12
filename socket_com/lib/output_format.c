a195b919e31afca5a737dd2a95202a33797acdaa
belongtothenight <106849935+belongtothenight@users.noreply.github.com>
2023-12-12 18:26:14 +0800

    initialize output format library

---
+++ socket_com/lib/output_format.c
@@ -0,0 +1,73 @@
+/**
+ * @file output_format.c
+ * @brief Output format settings
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "output_format.h"
+
+/// Maximum number of characters in a format buffer string
+#define char_buffer_size 256
+
+output_format_t *init_output_format(char *msg)
+{
+	char buf[char_buffer_size];
+
+	output_format_t *pFormat;
+	// formats
+	pFormat->reset = "\033[0m";
+	pFormat->bold = "\033[1m";
+	pFormat->red_foreground = "\033[31m";
+	pFormat->green_foreground = "\033[32m";
+	pFormat->yellow_foreground = "\033[33m";
+	// integrated formats
+	snprintf(buf,
+			sizeof(buf),
+			"%s%s%s%s%s%s",
+			pFormat->bold,
+			pFormat->green_foreground,
+			"[",
+			msg,
+			"] ",
+			pFormat->reset);
+	pFormat->info = strdup(buf);
+	snprintf(buf,
+			sizeof(buf),
+			"%s%s%s%s%s%s",
+			pFormat->bold,
+			pFormat->green_foreground,
+			"[",
+			msg,
+			"-SUCCESS] ",
+			pFormat->reset);
+	pFormat->success = strdup(buf);
+	snprintf(buf,
+			sizeof(buf),
+			"%s%s%s%s%s%s",
+			pFormat->bold,
+			pFormat->yellow_foreground,
+			"[",
+			msg,
+			"-WARN] ",
+			pFormat->reset);
+	pFormat->warn = strdup(buf);
+	snprintf(buf,
+			sizeof(buf),
+			"%s%s%s%s%s%s",
+			pFormat->bold,
+			pFormat->red_foreground,
+			"[",
+			msg,
+			"-ERROR] ",
+			pFormat->reset);
+	pFormat->error = strdup(buf);
+	// actions
+	pFormat->reset_terminal = "\033c";
+	pFormat->clear_screen_below = "\033[0J";
+	pFormat->move_cursor_up = "\033[1A";
+	pFormat->delete_line = "\033[0M";
+
+	return pFormat;
+}
---
+++ socket_com/lib/output_format.h
@@ -0,0 +1,51 @@
+/**
+ * @file output_format.h
+ * @brief Header file for otuput_format.c
+ * Ref:
+ * 1. https://github.com/belongtothenight/PRML_Code/blob/main/src/libc/output_format.h
+ */
+
+#ifndef OUTPUT_FORMAT_H
+#define OUTPUT_FORMAT_H
+
+/**
+ * @brief Output format struct for "socket_com" project
+ * @param reset reset all formats
+ * @param bold bold format
+ * @param red_foreground red foreground format
+ * @param green_foreground green foreground format
+ * @param yellow_foreground yellow foreground format
+ * @param info info message
+ * @param success success message
+ * @param warning warning message
+ * @param error error message
+ * @param reset_terminal reset terminal
+ * @param clear_screen_below clear screen below
+ * @param move_cursor_up move cursor up
+ * @param delete_line delete line
+ */
+typedef struct{
+	// formats
+	char *reset;
+	char *bold;
+	char *red_foreground;
+	char *green_foreground;
+	char *yellow_foreground;
+	// integrated formats
+	char *info;
+	char *success;
+	char *warning;
+	char *error;
+	// actions
+	char *reset_terminal;
+	char *clear_screen_below;
+	char *move_cursor_up;
+	char *delete_line;
+} output_format_t;
+
+/**
+ * @brief Initialize output format
+ * @param msg Message to print between brackets
+ * @return output format
+ */
+output_format_t *init_output_format(char *msg);
