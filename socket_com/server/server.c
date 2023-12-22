7b17aa9053f0a21cbdf4a08ad3cd228506202516
belongtothenight <106849935+belongtothenight@users.noreply.github.com>
2023-12-22 19:04:37 +0800

    Update server.c

--- socket_com/server/server.c
+++ socket_com/server/server.c
@@ -84,7 +84,7 @@ int main(int argc, char *argv[]) {
 	// Main process --> client connection
 	while (1) {
 		cfd = accept(sfd, (struct sockaddr *) &caddr, &caddr_len);
-		if (inet_ntop(AF_INET, &caddr.sin_addr.s_addr, ipv4_addr_str, INET_ADDRSTRLEN) == NULL) {
+		if (inet_ntop(AF_INET, &caddr, ipv4_addr_str, INET_ADDRSTRLEN) == NULL) {
 			printf("%sError converting address: %s\n", pFormat.error, ipv4_addr_str);
 			printf("%s%s\n", pFormat.error, strerror(errno));
 			LIB_SYSTEM_exit_program(1, pFormat);
