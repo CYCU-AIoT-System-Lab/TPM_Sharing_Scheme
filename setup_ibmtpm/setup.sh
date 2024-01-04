ccec2f58df52d691d00a7277d66ea666b3fa3f4e
belongtothenight <106849935+belongtothenight@users.noreply.github.com>
2024-01-04 16:15:33 +0800

    Update setup.sh

--- setup_ibmtpm/setup.sh
+++ setup_ibmtpm/setup.sh
@@ -468,7 +468,7 @@ active_ACS_Demo_Client () {
     if [ $acsClientMode == 1 ]; then
         echo_notice "setup_ibmtpm" "setup-active_ACS_Demo_Client" "Activating ACS Demo on local machine ..."
         #./clientenroll -alg rsa -v -ho ${acs_demo_server_ip} -co akcert.pem >| ${acs_demo_client_log_dir}
-        #export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${path_ibmtss}/utils" # Adjust shared library path
+        export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${path_ibmtss}/utils" # Adjust shared library path
         sudo $sudo_gflag ./clientenroll -alg rsa -v -ho ${acs_demo_server_ip} -co akcert.pem | sudo tee ${acs_demo_client_log_dir} > /dev/null
     elif [ $acsClientMode == 2 ]; then
         echo_notice "setup_ibmtpm" "setup-active_ACS_Demo_Client" "Activating ACS Demo on remote machine ..."
