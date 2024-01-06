ef402b220497168f0f346aa0b7255d1d88888531
belongtothenight <106849935+belongtothenight@users.noreply.github.com>
2024-01-06 16:46:43 +0800

    Make it not removing link files

--- setup_ibmtpm/remove.sh
+++ setup_ibmtpm/remove.sh
@@ -5,7 +5,7 @@ parse "./config.ini" "display"
 
 clear_dir $base_dir
 clear_dir "$html_dir/../"
-clear_dir $c_json_lib_dir "rmdir"
+clear_dir $tpm_data_dir "rmdir"
 sudo mysql -Bse "DROP DATABASE IF EXISTS $mysql_database;"
 
 echo_notice "setup_ibmtpm" "remove" "Done"
