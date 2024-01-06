637761bbcdb7af08ff66d5dcb62d5d8ef4c9c259
belongtothenight <106849935+belongtothenight@users.noreply.github.com>
2024-01-06 16:37:31 +0800

    Adjust syntax

--- common/setup.sh
+++ common/setup.sh
@@ -123,7 +123,7 @@ if [ $setup_ibmtpm -eq 1 ]; then
     echo_notice "common" "setup" "Running ibmtpm setup..."
     cd ../setup_ibmtpm
     sudo bash ./setup_sudo.sh
-else if [ $setup_ibmtpm -eq 2 ]; then
+elif [ $setup_ibmtpm -eq 2 ]; then
     echo_notice "common" "setup" "Running ibmtpm setup..."
     cd ../setup_ibmtpm
     ./setup.sh
