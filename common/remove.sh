044845a1e12ce7295475ece8e73337ab7d1bee57
belongtothenight <106849935+belongtothenight@users.noreply.github.com>
2024-01-14 23:13:06 +0800

    Re-try clear_dir

--- common/remove.sh
+++ common/remove.sh
@@ -7,9 +7,9 @@ check_var cmake_dir 1
 check_var valgrind_dir 1
 echo_notice "common" "remove" "Var check done"
 
-clear_dir apport_dir "rmdir"
-clear_dir cmake_dir "rmdir"
-clear_dir valgrind_dir "rmdir"
+clear_dir $apport_dir "rmdir"
+clear_dir $cmake_dir "rmdir"
+clear_dir $valgrind_dir "rmdir"
 echo_notice "common" "remove" "Clear dir done"
 
 unset apport_dir
