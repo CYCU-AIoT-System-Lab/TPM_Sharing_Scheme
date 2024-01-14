c6d5f6ade502dd47af1fdac1142bc8e903abb6e0
belongtothenight <106849935+belongtothenight@users.noreply.github.com>
2024-01-14 23:56:37 +0800

    Update function_common.sh

--- common/function_common.sh
+++ common/function_common.sh
@@ -5,18 +5,17 @@
 # Input variable: $1: common/function.sh
 #                 $2: config.ini
 load_preset () {
-    echo_notice "common" "setup" "Loading preset..."
+    # Load common function
     source "$1"
     echo_notice "common" "setup" "Env var to config file..."
+    # Replace all ${HOME} with $HOME (in config file)
     sed -i 's@${HOME}@'"$HOME"'@' "$2" # replace ${HOME} with $HOME
-    echo_notice "common" "setup" "Loading config..."
+    # Load config
     parse "$2" "display"
-    echo_notice "common" "setup" "Checking var..."
+    # Check var
     check_var install_platform 1
 }
-echo_notice "common" "function" "Loaded function: load_preset"
 
 clear_preset () {
-    echo_notice "common" "remove" "Clearing preset..."
+    echo "hi"
 }
-echo_notice "common" "function" "Loaded function: clear_preset"
