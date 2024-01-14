8f0ed5064232578d366d9112fc55246114356cab
belongtothenight <106849935+belongtothenight@users.noreply.github.com>
2024-01-15 00:07:47 +0800

    Update function_common.sh

--- common/function_common.sh
+++ common/function_common.sh
@@ -5,10 +5,10 @@
 # Input variable: $1: config.ini
 load_preset () {
     echo_notice "common" "setup" "Env var to config file..."
-    sed -i 's@${HOME}@'"$HOME"'@' "$2" # replace ${HOME} with $HOME
+    sed -i 's@${HOME}@'"$HOME"'@' "$1" # replace ${HOME} with $HOME
 
     echo_notice "common" "setup" "Loading config file..."
-    parse "$2" "display"
+    parse "$1" "display"
 
     echo_notice "common" "setup" "Checking var..."
     check_var install_platform 1
