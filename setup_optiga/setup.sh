1df75abf0ed83a947c985827c20fdafb141b88aa
belongtothenight <106849935+belongtothenight@users.noreply.github.com>
2024-01-17 15:40:08 +0800

    Update setup.sh

--- setup_optiga/setup.sh
+++ setup_optiga/setup.sh
@@ -4,15 +4,23 @@ source "../common/function.sh"
 source "./function_optiga.sh"
 load_preset "./config.ini"
 
-echo_notice "common" "setup" "Cloning optiga-tpm-explorer..."
+echo_notice "setup_optiga" "setup" "Cloning optiga-tpm-explorer..."
 git clone "$optiga_url"
 
-echo_notice "common" "setup" "Cancelling auto reboot..."
+echo_notice "setup_optiga" "setup" "Cancelling auto reboot..."
 cd ./optiga-tpm-explorer
 sed -i 's/sudo reboot/#sudo reboot/g' ./installation_script.sh
 
-echo_notice "common" "setup" "Running optiga-tpm-explorer installation script..."
+boot_config="/boot/config.txt"
+echo_notice "setup_optiga" "setup" "Configuring $boot_config..."
+sudo sed -i '70 i \
+
+' $boot_config
+sudo sed -i '71 i # Enable TPM' $boot_config
+sudo sed -i '72 i dtoverlay=tpm-slb9670' $boot_config
+
+echo_notice "setup_optiga" "setup" "Running optiga-tpm-explorer installation script..."
 bash ./installation_script.sh
 
-echo_notice "common" "setup" "Reboot to finish installation"
+echo_notice "setup_optiga" "setup" "Reboot to finish installation"
 clear_preset
