77b4d7cb4c8e565dc008a9a323685b5af6c1d954
belongtothenight <106849935+belongtothenight@users.noreply.github.com>
2024-01-19 16:56:58 +0800

    Remove wrong echo message

--- setup_optiga/remove.sh
+++ setup_optiga/remove.sh
@@ -4,7 +4,7 @@ source "../common/functions.sh"
 source "./function_optiga.sh"
 load_preset "./config.ini"
 
-echo_notice "common" "remove" "Removing directories..."
+echo_notice "setup_optiga" "remove" "Removing directories..."
 clear_dir "./optiga-tpm-explorer" "rmdir"
 
 clear_preset
