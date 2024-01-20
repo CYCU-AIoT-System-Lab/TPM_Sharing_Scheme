b2e4672e3ff3ae9dab3b94d1caf4b40ecab83ff0
belongtothenight <106849935+belongtothenight@users.noreply.github.com>
2024-01-20 23:45:47 +0800

    Add tools and fix indentation

--- common/setup.sh
+++ common/setup.sh
@@ -8,88 +8,90 @@ load_preset "./config.ini"
 
 build_cmake () {
     echo_notice "common" "setup" "Building ${BOLD}${GREEN}cmake${END}..."
-	mkdir -p $cmake_dir
-	cd $cmake_dir
-	wget $wget_gflag "https://cmake.org/files/v${cmake_ver}/cmake-${cmake_ver}.${cmake_build}.tar.gz"
-	tar -zxf "cmake-${cmake_ver}.${cmake_build}.tar.gz"
-	cd "cmake-${cmake_ver}.${cmake_build}"
-	./bootstrap
-	make $make_gflag -j$(nproc)
-	sudo $sudo_gflag make $make_gflag install
-	cmake --version
+    mkdir -p $cmake_dir
+    cd $cmake_dir
+    wget $wget_gflag "https://cmake.org/files/v${cmake_ver}/cmake-${cmake_ver}.${cmake_build}.tar.gz"
+    tar -zxf "cmake-${cmake_ver}.${cmake_build}.tar.gz"
+    cd "cmake-${cmake_ver}.${cmake_build}"
+    ./bootstrap
+    make $make_gflag -j$(nproc)
+    sudo $sudo_gflag make $make_gflag install
+    cmake --version
 }
 
 build_valgrind () {
     echo_notice "common" "setup" "Building ${BOLD}${GREEN}valgrind${END}..."
-	mkdir -p $valgrind_dir
-	cd $valgrind_dir
-	wget $wget_gflag "https://sourceware.org/pub/valgrind/valgrind-${valgrind_ver}.tar.bz2"
-	tar xfj "valgrind-${valgrind_ver}.tar.bz2"
-	cd "valgrind-${valgrind_ver}"
-	./configure
-	make $make_gflag -j$(nproc)
-	sudo $sudo_gflag make $make_gflag install
+    mkdir -p $valgrind_dir
+    cd $valgrind_dir
+    wget $wget_gflag "https://sourceware.org/pub/valgrind/valgrind-${valgrind_ver}.tar.bz2"
+    tar xfj "valgrind-${valgrind_ver}.tar.bz2"
+    cd "valgrind-${valgrind_ver}"
+    ./configure
+    make $make_gflag -j$(nproc)
+    sudo $sudo_gflag make $make_gflag install
 }
 
 install_req () {
     echo_notice "common" "setup" "Installing required packages..."
-	sudo apt-get $apt_gflag update
-	sudo apt-get $apt_gflag upgrade -y
-	aptins "git"
-	aptins "htop"
-	aptins "iftop"
-	aptins "curl"
-	aptins "wget"
-	aptins "net-tools"
-	aptins "neovim"
-	aptins "build-essential"
-	aptins "gcc"
-	aptins "make"
-	#aptins "libssl-dev" # Version 
+    sudo apt-get $apt_gflag update
+    sudo apt-get $apt_gflag upgrade -y
+    aptins "git"
+    aptins "htop"
+    aptins "s-tui"
+    aptins "plocate"
+    aptins "iftop"
+    aptins "curl"
+    aptins "wget"
+    aptins "net-tools"
+    aptins "neovim"
+    aptins "build-essential"
+    aptins "gcc"
+    aptins "make"
+    #aptins "libssl-dev" # Version 
     aptins "moreutils"
-	if [ ${install_platform} -eq 1 ] || [ ${install_platform} -eq 4 ]; then
-		aptins "libtool"
-		aptins "autoconf"
-		aptins "unzip"
-		build_cmake
-		cd $working_dir
-	fi
-	build_valgrind
-	cd $working_dir
+    if [ ${install_platform} -eq 1 ] || [ ${install_platform} -eq 4 ]; then
+        aptins "libtool"
+        aptins "autoconf"
+        aptins "unzip"
+        build_cmake
+        cd $working_dir
+    fi
+    build_valgrind
+    cd $working_dir
 }
 
 config_nvim () {
     echo_notice "common" "setup" "Configuring neovim..."
-	mkdir -p $nvim_dir
-	wget $wget_gflag "$nvim_config_url" -O "${nvim_dir}/init.vim"
+    mkdir -p $nvim_dir
+    wget $wget_gflag "$nvim_config_url" -O "${nvim_dir}/init.vim"
 }
 
 change_all_sh_mod () {
     echo_notice "common" "setup" "Changing all .sh files to executable..."
-	find .. -type f -iname "*.sh" -exec sudo chmod +x {} \;
+    find .. -type f -iname "*.sh" -exec sudo chmod +x {} \;
 }
 
 update_src () {
     echo_notice "common" "setup" "Pulling latest repo source..."
-	git stash
-	git stash clear
-	git pull
+    git stash
+    git stash clear
+    git pull
 }
 
 config_apport () {
     echo_notice "common" "setup" "Configuring apport..."
-	ulimit -c unlimited
-	mkdir -p $apport_dir
-	touch $apport_dir/settings
-	echo -e "[main]\nunpackaged=true\n" > $apport_dir/settings
-	rm -rf /var/crash/*
-	sudo service whoopsie stop
+    ulimit -c unlimited
+    mkdir -p $apport_dir
+    touch $apport_dir/settings
+    echo -e "[main]\nunpackaged=true\n" > $apport_dir/settings
+    rm -rf /var/crash/*
+    sudo service whoopsie stop
     echo_notice "common" "setup" "Core dumps will be generated in /var/crash"
 }
 
 reload_term () {
     echo_notice "common" "setup" "Reloading terminal..."
-	source ~/.bashrc
+    source ~/.bashrc
 }
 
 enable_ssh () {
@@ -156,8 +158,8 @@ fi
 cd $working_dir
 if [ $job_socket_com -eq 1 ]; then
     echo_notice "common" "setup" "Running socket_com setup..."
-	cd ../socket_com
-	install_platform=$install_platform bash ./setup.sh
+    cd ../socket_com
+    install_platform=$install_platform bash ./setup.sh
 else
     echo_warn "common" "setup" "Invalid Argument: $job_socket_com ! Skipping setup_socket_com..."
 fi
