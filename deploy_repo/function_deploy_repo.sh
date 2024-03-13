ddab5dcc6cf95f06eb0919f8208c49a055040704
belongtothenight <106849935+belongtothenight@users.noreply.github.com>
2024-03-13 14:34:42 +0800

    Add script to deploy custom repo

---
+++ deploy_repo/function_deploy_repo.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# common/function.sh is required to be sourced before using functions in this file
+
+verbose=0 # 0: no verbose, 1: verbose
+exit_code=1
+
+dirname="deploy_repo"
+filename="function_deploy_repo"
+
+load_preset () {
+    #if [ $verbose == 1 ]; then
+    #    echo_notice "${dirname}" "${filename}" "Env var to config file..."
+    #fi
+
+    if [ $verbose == 1 ]; then
+        echo_notice "${dirname}" "${filename}" "Loading config file..."
+    fi
+    parse "$1" ""
+
+    if [ $verbose == 1 ]; then
+        echo_notice "${dirname}" "${filename}" "Checking var..."
+    fi
+    check_var repo_ssh_link $exit_code
+    check_var pip_dep $exit_code
+    check_var main_path $exit_code
+    check_var deploy_repo $exit_code
+    check_var exec_main $exit_code
+}
+if [ $verbose == 1 ]; then
+    echo_notice "${dirname}" "${filename}" "Loaded function: load_preset"
+fi
+
+clear_preset () {
+    if [ $verbose == 1 ]; then
+        echo_notice "${dirname}" "${filename}" "Clearing preset..."
+    fi
+    unset repo_ssh_link
+    unset pip_dep
+    unset main_path
+    unset deploy_repo
+    unset exec_main
+}
+if [ $verbose == 1 ]; then
+    echo_notice "${dirname}" "${filename}" "Loaded function: clear_preset"
+fi
---
+++ deploy_repo/setup.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+source "../common/functions.sh"
+source "./function_deploy_repo"
+load_preset "./config.ini"
+
+dirname="deploy_repo"
+filename="setup"
+
+deploy_repo () {
+    echo_notice "${dirname}" "${filename}-deploy_repo" "Verifying SSH link ..."
+    if [[ $repo_ssh_link == git@github.com:* ]]; then
+        :
+    else
+        echo_error "${dirname}" "${filename}-deploy_repo" "SSH link is not valid"
+        exit 1
+    fi
+
+    echo_notice "${dirname}" "${filename}-deploy_repo" "Cloning repository ..."
+    err_retry_exec "git clone $repo_ssh_link" 1 5 "${dirname}" "${filename}-deploy_repo"
+
+    echo_notice "${dirname}" "${filename}-deploy_repo" "Installing dependencies ..."
+    err_retry_exec "pip install ${pip_dep}" 1 5 "${dirname}" "${filename}-deploy_repo"
+}
+
+exec_main () {
+    echo_notice "${dirname}" "${filename}" "Running main script ..."
+    python ${main_path}
+}
+
+echo_notice "${dirname}" "${filename}" "Running setup script ..."
+
+if [ $deploy_repo == 1 ]; then deploy_repo; fi
+if [ $exec_main   == 1 ]; then exec_main;   fi
+
+clear_preset
+
+echo_notice "${dirname}" "${filename}" "Setup complete"
