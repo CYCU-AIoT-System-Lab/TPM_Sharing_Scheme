38b54f7ccbda98c23586c4400e4c3473a6e9cbf9
belongtothenight <106849935+belongtothenight@users.noreply.github.com>
2024-03-14 17:16:32 +0800

    Add remove.sh for deploy_repo

---
+++ deploy_repo/remove.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+source "../common/functions.sh"
+source "./function_deploy_repo.sh"
+load_preset "./config.ini"
+
+dirname="deploy_repo"
+filename="remove"
+
+clear_dir "mmWAVE_Radar"
+
+clear_preset
