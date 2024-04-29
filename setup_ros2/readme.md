0592f5f2987aeef25a452f231fa5bc8bf412d3b3
belongtothenight <106849935+belongtothenight@users.noreply.github.com>
2024-04-29 11:29:07 +0800

    Testing ros2 jetson nano installation process

--- setup_ros2/readme.md
+++ setup_ros2/readme.md
@@ -2,6 +2,11 @@
 
 For raspberry pi platform, docker is required; for jetson nano, use normal binary installation.
 
+ROS2 Humble is prefered then Iron for longer support (LTS 2027).
+
 ## References
 
-1. https://docs.ros.org/en/foxy/How-To-Guides/Installing-on-Raspberry-Pi.html
+1. [https://docs.ros.org/en/foxy/How-To-Guides/Installing-on-Raspberry-Pi.html](https://docs.ros.org/en/foxy/How-To-Guides/Installing-on-Raspberry-Pi.html) 
+2. [https://robotics.stackexchange.com/questions/105772/which-ros2-distro-iron-or-humble](https://robotics.stackexchange.com/questions/105772/which-ros2-distro-iron-or-humble)
+3. [https://docs.ros.org/en/dashing/Installation/Ubuntu-Install-Binary.html](https://docs.ros.org/en/dashing/Installation/Ubuntu-Install-Binary.html)
+4. [https://github.com/ros2/ros2/releases](https://github.com/ros2/ros2/releases)
---
+++ setup_ros2/setup_jetson.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+wget -nv --show-progress https://github.com/ros2/ros2/releases/download/release-humble-20240222/ros2-humble-20240222-linux-jammy-arm64.tar.bz2 --output-document ros2-humble-20240222-linux-jammy-arm64.tar.bz2
+tar -xf ros2-humble-20240222-linux-jammy-arm64.tar.bz2 -C .
+cd ros2-linux
+sudo apt install -y python-rosdep
+sudo rosdep init
+rosdep update
+sudo apt install python-pip
+sudo pip install -U rosdep rosinstall_generator wstool rosinstall
+curl -s https://packagecloud.io/install/repositories/dirk-thomas/vcstool/script.deb.sh | sudo bash
+#sudo apt install python3-vcstool
+rosdep install --from-paths share --ignore-src --rosdistro humble -y --skip-keys "fastcdr fastrtps rmw_connextdds iceoryx_binding_c urdfdom_headers ignition-math6 ignition-cmake2 python3-pykdl cyclonedds python3-lark-parser python3-rosdistro-modules python3-catkin-pkg-modules"
+sudo apt install -y libpython3-dev python3-pip
+pip3 install -U argcomplete
