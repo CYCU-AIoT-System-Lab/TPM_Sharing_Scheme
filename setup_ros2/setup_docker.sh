7b842691efa181635e780c3968f6d3b818a2ccae
belongtothenight <106849935+belongtothenight@users.noreply.github.com>
2024-05-09 21:45:27 +0800

    Update setup_docker.sh

--- setup_ros2/setup_docker.sh
+++ setup_ros2/setup_docker.sh
@@ -8,8 +8,8 @@ echo "Pulling from source"
 sudo docker pull $ros2_image
 
 echo "Build new project image from Dockerfile"
-#sudo docker build --build-arg PACKAGE_NAME=$proj_name -t ${proj_name}_pkg_image .
+sudo docker build --build-arg PACKAGE_NAME=$proj_name -t ${proj_name}_pkg_image .
 
 echo "Create new container from image"
 sudo docker run -it --name ${proj_name}_container ${proj_name}_pkg_image
-#sudo docker run --name ${proj_name}_container ${proj_name}_pkg_image
+sudo docker run --name ${proj_name}_container ${proj_name}_pkg_image
