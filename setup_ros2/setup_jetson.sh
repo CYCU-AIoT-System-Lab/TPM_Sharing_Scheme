8e936a48fbd2699a31aace1dd719ea0c27291807
belongtothenight <106849935+belongtothenight@users.noreply.github.com>
2024-05-10 13:45:32 +0800

    try installation script

--- setup_ros2/setup_jetson.sh
+++ setup_ros2/setup_jetson.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
+set +x
 
+sudo apt install -y curl
 wget -nv --show-progress https://github.com/ros2/ros2/releases/download/release-humble-20240222/ros2-humble-20240222-linux-jammy-arm64.tar.bz2 --output-document ros2-humble-20240222-linux-jammy-arm64.tar.bz2
 tar -xf ros2-humble-20240222-linux-jammy-arm64.tar.bz2 -C .
 cd ros2-linux
 sudo apt install -y python-rosdep
 sudo rosdep init
 rosdep update
-sudo apt install python-pip
+sudo apt install -y python-pip
 sudo pip install -U rosdep rosinstall_generator wstool rosinstall
 curl -s https://packagecloud.io/install/repositories/dirk-thomas/vcstool/script.deb.sh | sudo bash
 #sudo apt install python3-vcstool
