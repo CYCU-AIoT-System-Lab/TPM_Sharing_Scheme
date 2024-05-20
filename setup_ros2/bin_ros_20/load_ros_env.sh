e655ca51382c7a061f10bc03c384324d7fefb8c7
belongtothenight <106849935+belongtothenight@users.noreply.github.com>
2024-05-20 22:18:14 +0800

    Update load_ros_env.sh

--- setup_ros2/bin_ros_20/load_ros_env.sh
+++ setup_ros2/bin_ros_20/load_ros_env.sh
@@ -5,7 +5,7 @@ source $HOME/venv/bin/activate || echo "> Python 3.8 virtual environment not fou
 echo "> Trying to source ROS2 workspace setup.bash, if it exists..."
 ros_source="${ros_distro_ws}/ros2-linux/setup.bash" && source $ros_source || \
     ros_source="${ros_distro_ws}/install/*setup.bash" && source $ros_source || \
-    ros_source="/opt/ros/$ros_distro/setup.bash"
+    ros_source="/opt/ros/$ros_distro/setup.bash" && source $ros_source || \
     ros_source="${ros_distro_ws}/local_setup.bash" && source $ros_source || \
     { echo "ROS is not installed in default location \"~/ros2_${ros_distro} or not installed!"; exit 1; }
 
