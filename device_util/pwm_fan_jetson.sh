1609015b43fe4fe489a00760c3f31cb339712f00
belongtothenight <106849935+belongtothenight@users.noreply.github.com>
2024-05-21 19:51:05 +0800

    Add a jetson nano pwm fan control script with cli

---
+++ device_util/pwm_fan_jetson.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+min_num=0
+max_num=255
+
+cli_input_str="$@"
+cli_input_arr=($cli_input_str)
+re='^[0-9]+$'
+
+help_msg () {
+    echo "> Usage:  ./pwm_fan_jetson.sh <pwm_number>"
+    echo "          $min_num ~ <pwm_number> ~ $max_num"
+    exit 0
+}
+
+if [ -z "${cli_input_str}" ]; then
+    help_msg
+    exit 0
+fi
+
+if [ $cli_input_str == "-h" ] || [ $cli_input_str == "--help" ]; then
+    help_msg
+    exit 0
+fi
+
+if ! [[ ${cli_input_arr[0]} =~ $re ]]; then
+    echo "> Input: $@ | Use numbers between $min_num to $max_num"
+    exit 1
+fi
+
+if [ ${cli_input_arr[0]} -lt $min_num ] || [ ${cli_input_arr[0]} -gt $max_num ]; then
+    echo "> Input: $@ | Use numbers between $min_num to $max_num"
+    exit 1
+else
+    exec_cmd_arr=(sudo sh -c "\"echo ${cli_input_arr[0]} > /sys/devices/pwm-fan/target_pwm\"")
+    echo "> Executing -> ${exec_cmd_arr[@]}"
+    "${exec_cmd_arr[@]}" || { echo "PWM fan inaccessable"; exit 1; }
+fi
