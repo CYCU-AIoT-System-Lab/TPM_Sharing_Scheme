f6b2b685066c97b8d854be70e705aefd1619143c
belongtothenight <106849935+belongtothenight@users.noreply.github.com>
2024-05-21 20:03:06 +0800

    Add ups setup script and remove original documentation on how to use it

--- device_util/readme.md
+++ device_util/readme.md
@@ -3,3 +3,5 @@
 ## Structure
 
 - [./upgrade_jetson](./upgrade_jetson): Upgrade Jetpack Ubuntu 18.04 to 20.04.
+- [./pwm_fan_jetson.sh](./pwm_fan_jetson.sh): Control Jetson Nano PWM fan.
+- [./ups_waveshare_jetson.sh](./ups_waveshare_jetson.sh): Setup waveshare Jetson Nano UPS display monitor.
---
+++ device_util/ups_waveshare_jetson.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Ref: https://www.waveshare.com/ups-power-module.htm
+# Ref: --> https://blog.cavedu.com/2019/10/04/nvidia-jetson-nano-fan/
+
+install_dir="/opt"
+
+echo "> Updating apt ..."
+sudo apt update && sudo apt upgrade -y
+
+echo "> Installing from debian package ..."
+sudo apt-get install python3-smbus -y
+sudo apt-get install python-smbus -y
+
+echo "> Cloning from waveshare github repository ..."
+cd $install_dir
+git clone https://github.com/waveshare/UPS-Power-Module
+
+echo "> Final installing ..."
+cd UPS-Power-Module
+sudo ./install.sh
+
+echo "> Installation finished!"
