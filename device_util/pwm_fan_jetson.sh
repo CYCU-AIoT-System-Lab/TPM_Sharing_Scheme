99b82ad169ce38606bd2fa373f537f4b6056e28f
belongtothenight <106849935+belongtothenight@users.noreply.github.com>
2024-05-22 15:47:58 +0800

    Switch input value from pwm to percentage

--- device_util/pwm_fan_jetson.sh
+++ device_util/pwm_fan_jetson.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
 min_num=0
-max_num=255
-
+max_num=100
 cli_input_str="$@"
 cli_input_arr=($cli_input_str)
 re='^[0-9]+$'
 
 help_msg () {
     echo "> This is a script to control Jetson Nano PWM fan with Linux device utility"
-    echo "> Usage:  ./pwm_fan_jetson.sh <pwm_number>"
-    echo "          $min_num ~ <pwm_number> ~ $max_num"
+    echo "> Usage:  ./pwm_fan_jetson.sh <pwm_percentage>"
+    echo "          $min_num ~ <pwm_percentage> ~ $max_num"
     exit 0
 }
 
@@ -34,8 +33,9 @@ if [ ${cli_input_arr[0]} -lt $min_num ] || [ ${cli_input_arr[0]} -gt $max_num ];
     echo "> Input: $@ | Use numbers between $min_num to $max_num"
     exit 1
 else
-    exec_cmd_arr=(sudo sh -c "echo ${cli_input_arr[0]} > /sys/devices/pwm-fan/target_pwm")
+    pwm_value=$((cli_input_arr[0] * 255 / (max_num - min_num)))
+    exec_cmd_arr=(sudo sh -c "echo $pwm_value > /sys/devices/pwm-fan/target_pwm")
     echo "> Executing -> ${exec_cmd_arr[@]}"
     "${exec_cmd_arr[@]}" || { echo "PWM fan inaccessable"; exit 1; }
-    echo "> PWM fan speed set to $((cli_input_arr[0] * 100 / (max_num - min_num)))%"
+    echo "> PWM fan speed set to ${cli_input_arr[0]}%"
 fi
