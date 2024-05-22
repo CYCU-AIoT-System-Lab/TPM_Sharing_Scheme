4d0c09a97fede0821f3b5d28f4176ea3da108a35
belongtothenight <106849935+belongtothenight@users.noreply.github.com>
2024-05-22 20:02:52 +0800

    Update upgrade_jetson_1.sh

--- device_util/upgrade_jetson_18/upgrade_jetson_1.sh
+++ device_util/upgrade_jetson_18/upgrade_jetson_1.sh
@@ -3,6 +3,14 @@ set -x
 
 # Ref: https://qengineering.eu/install-ubuntu-20.04-on-jetson-nano.html
 
+echo "> Install apt-fast ..."
+echo "choose \"apt-get\" and the maximum number of connections"
+sudo add-apt-repository ppa:apt-fast/stable -y
+sudo apt-get install apt-fast
+echo "alias apt-get='apt-fast'" >> ~/.bash_aliases
+echo "alias apt='apt-fast'" >> ~/.bash_aliases
+source ~/.bash_aliases
+
 echo "> Update apt ..."
 sudo apt remove --purge -y chromium-browser chromium-browser-l10n
 sudo apt update
@@ -12,13 +20,6 @@ sudo apt autoremove -y
 sudo sed -i '/Prompt=never/c\Prompt=normal' /etc/update-manager/release-upgrades
 sudo apt update
 sudo apt dist-upgrade -y
-
-echo "> Install apt-fast ..."
-echo "choose \"apt-get\" and the maximum number of connections"
-sudo add-apt-repository ppa:apt-fast/stable -y
-sudo apt-get install apt-fast
-echo "alias apt-get='apt-fast'" >> ~/.bash_aliases
-echo "alias apt='apt-fast'" >> ~/.bash_aliases
 
 # Perform required reboot
 echo ""
