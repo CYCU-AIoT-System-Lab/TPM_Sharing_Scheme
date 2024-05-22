30dc7e2d0333df7e34be9239b0d1c7b5bae21d72
belongtothenight <106849935+belongtothenight@users.noreply.github.com>
2024-05-22 19:32:21 +0800

    Add apt-fast deployment

--- device_util/upgrade_jetson_18/upgrade_jetson_1.sh
+++ device_util/upgrade_jetson_18/upgrade_jetson_1.sh
@@ -3,6 +3,7 @@ set -x
 
 # Ref: https://qengineering.eu/install-ubuntu-20.04-on-jetson-nano.html
 
+echo "> Update apt ..."
 sudo apt remove --purge -y chromium-browser chromium-browser-l10n
 sudo apt update
 sudo apt install -y htop iftop net-tools
@@ -11,6 +12,11 @@ sudo apt autoremove -y
 sudo sed -i '/Prompt=never/c\Prompt=normal' /etc/update-manager/release-upgrades
 sudo apt update
 sudo apt dist-upgrade -y
+
+echo "> Install apt-fast ..."
+sudo add-apt-repository ppa:apt-fast/stable -y
+sudo apt-get install apt-fast
+echo "alias apt-get='apt-fast'" >> ~/.bash_aliases
 
 # Perform required reboot
 echo ""
