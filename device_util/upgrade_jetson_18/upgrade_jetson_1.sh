e9f9003ac306d2c36665bdbb8b8a3613a3cc4139
belongtothenight <106849935+belongtothenight@users.noreply.github.com>
2024-05-22 22:24:14 +0800

    Update upgrade_jetson_1.sh

--- device_util/upgrade_jetson_18/upgrade_jetson_1.sh
+++ device_util/upgrade_jetson_18/upgrade_jetson_1.sh
@@ -7,6 +7,7 @@ echo "> Install apt-fast ..."
 echo "choose \"apt-get\" and the maximum number of connections"
 sudo add-apt-repository ppa:apt-fast/stable -y
 sudo apt-get install apt-fast
+echo "alias apt-get='apt-fast'" >> ~/.bash_aliases
 echo "alias apt='apt-fast'" >> ~/.bash_aliases
 source ~/.bash_aliases
 
