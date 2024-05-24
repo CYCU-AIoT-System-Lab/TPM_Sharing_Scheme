074f03b6faed5c6d5d78f390e2fe30de0ae60ce9
belongtothenight <106849935+belongtothenight@users.noreply.github.com>
2024-05-24 19:18:35 +0800

    Update figure and complete uninstall ros2 script

--- setup_ros2/pac_ros_22/uninstall_ros.sh
+++ setup_ros2/pac_ros_22/uninstall_ros.sh
@@ -2,6 +2,14 @@
 
 echo "> Removing sources ..."
 sudo rm -rf /etc/apt/sources.list.d/ros2*.list
+sudo rm -rf /etc/ros/rosdep/sources.list.d/20-default.list
+sudo rm -rf $HOME/.ros
 
-echo "> Removing installed directories ..."
-sudo rm -rf /opt/ros
+echo "> Removing installed packages ..."
+sudo apt remove -y ros-humble-desktop
+sudo apt remove -y ros-humble-ros-base
+sudo apt remove -y ros-dev-tools
+sudo apt autoremove -y
+
+#echo "> Removing installed directories ..."
+#sudo rm -rf /opt/ros
