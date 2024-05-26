d1a86b64b909ef82d5d001f3b2557bba8dd599a2
belongtothenight <106849935+belongtothenight@users.noreply.github.com>
2024-05-26 19:18:36 +0800

    Add process to uninstall maked binaries and libraries, but ibm softwares doesn't provide target

--- setup_ibmtpm/remove.sh
+++ setup_ibmtpm/remove.sh
@@ -4,15 +4,25 @@ source "../common/functions.sh"
 source "./function_ibmtpm.sh"
 load_preset "./config.ini"
 
+#echo_notice "${dirname}" "${filename}" "Uninstalling dependencies..."
+#cd "${base_dir}/ibmacs${ibmacs_ver}/acs" && sudo make uninstall    # no uninstall target provided
+#cd "${base_dir}/ibmtpm${ibmtpm_ver}/src" && sudo make uninstall    # no uninstall target provided
+#cd "${base_dir}/ibmtss${ibmtss_ver}" && sudo make uninstall        # no uninstall target provided
+
+echo_notice "${dirname}" "${filename}" "Removing directories..."
 clear_dir "$(basename -- $html_dir)"
 clear_dir "$tpm_data_dir"
 clear_dir "${base_dir}/ibmacs${ibmacs_ver}"
 clear_dir "${base_dir}/ibmtpm${ibmtpm_ver}"
 clear_dir "${base_dir}/ibmtss${ibmtss_ver}"
+
+echo_notice "${dirname}" "${filename}" "Removing symlink..."
 cfer "${base_dir}/ibmtss" "setup_ibmtpm" "remove" "Removing old ibmtss symbolic link ..."
 cfer "${base_dir}/ibmtpm" "setup_ibmtpm" "remove" "Removing old ibmtpm symbolic link ..."
 cfer "${base_dir}/ibmacs" "setup_ibmtpm" "remove" "Removing old ibmacs symbolic link ..."
 cfer "${c_json_lib_lin_linkk_dir}" "setup_ibmtpm" "remove" "Removing old c_json_lib symbolic link ..."
+
+echo_notice "${dirname}" "${filename}" "Removing DB..."
 sudo mysql -Bse "DROP DATABASE IF EXISTS $mysql_database;"
 sudo mysql -Bse "DROP DATABASE IF EXISTS machines;"
 
--- setup_optiga/remove.sh
+++ setup_optiga/remove.sh
@@ -6,8 +6,16 @@ load_preset "./config.ini"
 
 dirname="setup_optiga"
 filename="remove"
+script=$(realpath "$0")
+script_path=$(dirname "$script")
+
+echo_notice "${dirname}" "${filename}" "Uninstalling dependencies..."
+cd "$script_path/optiga-tpm-explorer/tpm2-tss/" && sudo make uninstall
+cd "$script_path/optiga-tpm-explorer/tpm2-tools/" && sudo make uninstall
+cd "$script_path/optiga-tpm-explorer/tpm2-abrmd/" && sudo make uninstall
+cd "$script_path/optiga-tpm-explorer/tpm2-tss-engine/" && sudo make uninstall
 
 echo_notice "${dirname}" "${filename}" "Removing directories..."
-clear_dir "./optiga-tpm-explorer"
+clear_dir "$script_path/optiga-tpm-explorer"
 
 clear_preset
