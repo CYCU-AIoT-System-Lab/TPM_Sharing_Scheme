cd3d86e1a6c5c53e9baf3309186145396bc4ef7d
belongtothenight <106849935+belongtothenight@users.noreply.github.com>
2024-06-06 22:20:05 +0800

    [major] [multi] Rework on setup and mbc pipeline, using alternative approach with NVM, which should be done using C with tpm2tss for simplicity

--- boot/mbc_last.sh
+++ boot/mbc_last.sh
@@ -1,5 +1,73 @@
 #!/bin/bash
+#set -x
 
+TPM2_AUTH_NV="nv123"
+TPM2_NVM_INDEX="0x1500016"
+HASH_SCRIPT="./hash.sh"
+HASH_TARGET="dir_list.txt"
+HASH="sha256" # can be sha1, sha256
 PCR_IDX_INIT_ZERO=0
-PCR_X_1=""
-PCR_X=""
+PCR_IDX_MIN=0
+PCR_IDX_MAX=23
+INITIAL_ZERO_PCR="0000000000000000000000000000000000000000000000000000000000000000"
+INTEGRITY_CHECK_PASS=false
+
+# >0. Checking existance
+#    - TPM
+#    - scripts
+
+# >1. Find the index of last non-zero PCR
+echo "Finding the index of last non-zero PCR..."
+for i in $(seq $PCR_IDX_MIN $PCR_IDX_MAX); do
+    PCR_TEMP="$(tpm2_pcrread $HASH:$i)"
+    PCR_TEMP="${PCR_TEMP#*x}"
+    if [[ "$PCR_TEMP" == "$INITIAL_ZERO_PCR" ]]; then
+        PCR_IDX_INIT_ZERO=$((i-1))
+        break
+    fi
+done
+echo -e "Last non-zero PCR index: \t$PCR_IDX_INIT_ZERO"
+
+# >2. Read PCR out from TPM
+echo "Reading PCR from TPM..."
+PCR_IDX_INIT_ZERO_1=$((PCR_IDX_INIT_ZERO-1))
+if [[ $PCR_IDX_INIT_ZERO_1 -lt $PCR_IDX_MIN ]]; then
+    PCR_X_1="$INITIAL_ZERO_PCR"
+else
+    PCR_X_1="$(tpm2_pcrread $HASH:$PCR_IDX_INIT_ZERO_1)"
+    PCR_X_1="${PCR_X_1#*x}"
+fi
+PCR_X="$(tpm2_pcrread $HASH:$PCR_IDX_INIT_ZERO)"
+PCR_X="${PCR_X#*x}"
+echo -e "PCR_HASH[$PCR_IDX_INIT_ZERO_1]: \t$PCR_X_1"
+echo -e "PCR_HASH[$PCR_IDX_INIT_ZERO]: \t$PCR_X"
+
+# >3. Call hashing script
+echo "Hashing target file..."
+. "$HASH_SCRIPT" "$HASH_TARGET" "$PCR_X_1" 1> /dev/null
+FINAL_HASH_VALUE="$(echo $FINAL_HASH_VALUE | tr -d ' ')"
+echo -e "Generated PCR_HASH[$PCR_IDX_INIT_ZERO]: \t$FINAL_HASH_VALUE"
+
+# >4. Read hash out from NVM
+echo "Reading hash from NVM..."
+NVM_HASH_TMP_FILE="nvm_hash_value.txt.tmp"
+tpm2_nvread "$TPM2_NVM_INDEX" -s 64 -o 0 -P "$TPM2_AUTH_NV" -o "$NVM_HASH_TMP_FILE"
+NVM_HASH="$(cat $NVM_HASH_TMP_FILE)"
+rm "$NVM_HASH_TMP_FILE"
+echo -e "Hash value in NVM: \t$NVM_HASH"
+if [[ "$NVM_HASH" == "$FINAL_HASH_VALUE" ]]; then
+    echo "Hash value in NVM is the same as the generated one"
+    INTEGRITY_CHECK_PASS=true
+else
+    echo "Hash value in NVM is different from the generated one"
+fi
+
+# >5. Extend PCR index if needed
+if [[ "$INTEGRITY_CHECK_PASS" == true ]]; then
+    echo "No need to extend PCR, hash value is the same as the one previously calculated and stored in NVM"
+    echo "MBC: File integrity check completed successfully!"
+else
+    echo "Extending PCR..."
+    #tpm2_pcrextend "$PCR_IDX_INIT_ZERO:$HASH=$FINAL_HASH_VALUE"
+    echo "MBC: File integrity check failed!"
+fi
--- boot/setup_mbc_last.sh
+++ boot/setup_mbc_last.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
+#set -x
 
 # note: remember to add fail check
 
-PCR_IDX_INIT_ZERO=0
-HASH="sha256" # can be sha1, sha256
-
-
+TPM2_AUTH_OWNER="owner123"
+TPM2_AUTH_ENDORSEMENT="endorsement123"
+TPM2_AUTH_LOCKOUT="lockout123"
+TPM2_AUTH_ATTRIBUTE="authwrite|authread|read_stclear"
+TPM2_AUTH_NV="nv123"
+TPM2_NVM_INDEX="0x1500016"
 HASH_SCRIPT="./hash.sh"
 HASH_TARGET="dir_list.txt"
 MBC_SCRIPT="./mbc_last.sh"
+PCR_IDX_INIT_ZERO=0
+HASH="sha256" # can be sha1, sha256
 PCR_IDX_MIN=0
 PCR_IDX_MAX=23
 INITIAL_ZERO_PCR="0000000000000000000000000000000000000000000000000000000000000000"
@@ -17,7 +22,17 @@ INITIAL_ZERO_PCR="00000000000000000000000000000000000000000000000000000000000000
 #    - TPM
 #    - scripts
 
-# >1. Read PCR out from TPM and find the index of the first non-zero byte
+# >1. Configure TPM
+echo "Configuring TPM..."
+tpm2_startup -c
+tpm2_clear -c p
+tpm2_changeauth -c owner $TPM2_AUTH_OWNER
+tpm2_changeauth -c endorsement $TPM2_AUTH_ENDORSEMENT
+tpm2_changeauth -c lockout $TPM2_AUTH_LOCKOUT
+tpm2_nvdefine $TPM2_NVM_INDEX -C o -s 900 -a $TPM2_AUTH_ATTRIBUTE -P $TPM2_AUTH_OWNER -p $TPM2_AUTH_NV
+# now we can write to NVM
+
+# >2. Read PCR out from TPM and find the index of the first non-zero byte
 echo "Reading PCR from TPM..."
 PCR_X_1="$INITIAL_ZERO_PCR" # last non-zero PCR
 for i in $(seq $PCR_IDX_MIN $PCR_IDX_MAX); do
@@ -31,28 +46,27 @@ for i in $(seq $PCR_IDX_MIN $PCR_IDX_MAX); do
     #echo $PCR_TEMP
 done
 
-# >2. Call hashing script
+# >3. Call hashing script
 echo "Hashing target file..."
 . "$HASH_SCRIPT" "$HASH_TARGET" "$PCR_X_1" 1> /dev/null
 FINAL_HASH_VALUE="$(echo $FINAL_HASH_VALUE | tr -d ' ')"
-echo $FINAL_HASH_VALUE
+#echo $FINAL_HASH_VALUE
 echo -e "First zero PCR index: \t$PCR_IDX_INIT_ZERO"
-echo -e "Generated PCR[$PCR_IDX_INIT_ZERO]: \t$FINAL_HASH_VALUE"
+echo -e "Generated PCR_HASH[$PCR_IDX_INIT_ZERO]: \t$FINAL_HASH_VALUE"
 
-# >3. Extend PCR index x with new value
+# >4. Extend PCR index x with new value
 echo "Extending PCR..."
 tpm2_pcrextend "$PCR_IDX_INIT_ZERO:$HASH=$FINAL_HASH_VALUE"
 
-# >4. Read just extended PCR
+# >5. Read just extended PCR
 echo "Reading extended PCR..."
 PCR_X="$(tpm2_pcrread $HASH:$PCR_IDX_INIT_ZERO)"
 PCR_X="${PCR_X#*x}"
 
-# >5. Write PCR and index to MBC script
-echo "Writing PCR and index to MBC script..."
-sed -i "3s/PCR_IDX_INIT_ZERO=.*/PCR_IDX_INIT_ZERO=$PCR_IDX_INIT_ZERO/" "$MBC_SCRIPT"
-sed -i "4s/PCR_X_1=.*/PCR_X_1=\"$PCR_X_1\"/" "$MBC_SCRIPT"
-sed -i "5s/PCR_X=.*/PCR_X=\"$PCR_X\"/" "$MBC_SCRIPT"
-
-
+# >6. Write hash value to NVM
+echo "Writing hash to NVM..."
+HASH_TMP_FILE="hash_value.txt.tmp"
+echo "$FINAL_HASH_VALUE" > $HASH_TMP_FILE
+tpm2_nvwrite $TPM2_NVM_INDEX -P $TPM2_AUTH_NV -i $HASH_TMP_FILE
+rm $HASH_TMP_FILE
 echo "MBC script updated successfully!"
