5553b335f7536be4388bbe735b8faee8aae3ab06
belongtothenight <106849935+belongtothenight@users.noreply.github.com>
2024-07-08 11:19:11 +0800

    add client-side execution code

--- acs_routine/setup_acsroutine.sh
+++ acs_routine/setup_acsroutine.sh
@@ -43,15 +43,12 @@ setup_libtrace () {
 # install & compile SERVER program:
 #   - [O] ACS DB parsing
 #   - [O] detect anomaly client traffic
-#   - [ ] block anomaly client traffic
-server_setup_task12=1
+#   - [O] block anomaly client traffic
 server_setup () {
-    if [ $server_setup_task12 -eq 1 ]; then
-        echo_notice "$dirname" "$filename" "Compiling binaries for ACS Remote Attestation routine"
-        ./bootstrap.sh
-        ./configure
-        make
-    fi
+    echo_notice "$dirname" "$filename" "Compiling binaries for ACS Remote Attestation routine"
+    ./bootstrap.sh
+    ./configure
+    make
 }
 
 # execute SERVER program
@@ -101,15 +98,11 @@ server_exec () {
 #   - [O] send attestation info to server
 #   - [O] detect anomaly server traffic
 #   - [O] block anomaly server traffic
-client_setup_task1=0 # This is not neccesary since it is performed by setup_ibmtpm
 client_setup () {
-    if [ $client_setup_task1 -eq 1 ]; then
-        echo_notice "$dirname" "$filename" "Enrolling client"
-        sudo bash -c "\
-            cd $BASE_DIR/ibmacs; \
-            export LD_LIBRARY_PATH=\"$BASE_DIR/ibmtss/utils:$LD_LIBRARY_PATH\"; \
-            $BASE_DIR/ibmacs/clientenroll -alg ec -vv -ho $ACS_DEMO_SERVER_IP -ma $ACS_DEMO_CLIENT_IP -co akeccert.pem" || { echo "please inspect the error message! if it is \`already exists machine error\`, this can be ignored."; }
-    fi
+    echo_notice "$dirname" "$filename" "Compiling binaries for ACS Remote Attestation routine"
+    ./bootstrap.sh
+    ./configure
+    make
 }
 
 # execute CLIENT program
@@ -117,43 +110,54 @@ client_setup () {
 #   - [O] constant traffic monitoring
 #   - [O] block client if top one condition fails
 client_exec_approach=2
+client_exec_task1=1
+client_exec_task2=1
 client_exec () {
-    echo_notice "$dirname" "$filename" "client-side executing"
-    echo "client_exec_approach: $approach"
-    if [ $client_exec_approach -eq 1 ]; then
-        # this client_exec_approach tries to execute client only with systemd managed swtpm
-        # but it seems to need swtpm not handeled by systemd
-        p1="$BASE_DIR/ibmacs/tpm2bios.log"
-        p2="$BASE_DIR/ibmacs/imasig.log"
-        # cmd 1&2 can add "-v" flag for verbose
-            #$BASE_DIR/ibmtss/utils/createekcert -rsa 2048 -cakey cakey.pem -capwd rrrr -vv
-            #$BASE_DIR/ibmtss/utils/createekcert -ecc nistp256 -cakey cakeyecc.pem -capwd rrrr -caalg ec -vv
-            #$BASE_DIR/ibmtss/utils/nvread -ha 01c00002 | awk 'NR==1 {next} {print}' | xxd -r -ps | base64 | sed -e '1i -----BEGIN CERTIFICATE-----' -e '$a -----END CERTIFICATE-----' > VMW_EK_CACERT.pem
-            #$BASE_DIR/ibmtss/utils/nvread -ha 01c0000a | awk 'NR==1 {next} {print}' | xxd -r -ps | base64 | sed -e '1i -----BEGIN CERTIFICATE-----' -e '$a -----END CERTIFICATE-----' > VMW_EKECC_CACERT.pem
-            #$BASE_DIR/ibmtss/utils/eventextend -if $p1 -tpm; \
-            #$BASE_DIR/ibmtss/utils/imaextend -if $p2 -le; \
-        sudo bash -c "\
-            cd $BASE_DIR/ibmacs; \
-            export LD_LIBRARY_PATH=\"$BASE_DIR/ibmtss/utils:$LD_LIBRARY_PATH\"; \
-            $BASE_DIR/ibmacs/client -alg ec -vv -ifb $p1 -ifi $p2 -ho $ACS_DEMO_SERVER_IP -ma $ACS_DEMO_CLIENT_IP"
-    elif [ $client_exec_approach -eq 2 ]; then
-        # this client_exec_approach directly calls setup_ibmtpm for consistantly remote attestation
-        #1> disable swtpm daemon
-        sudo systemctl stop swtpm.service
+    if [ $client_exec_task1 -eq 1 ]; then
+        #echo "client_exec_approach: $approach"
+        if [ $client_exec_approach -eq 1 ]; then
+            # this client_exec_approach tries to execute client only with systemd managed swtpm
+            # but it seems to need swtpm not handeled by systemd
+            p1="$BASE_DIR/ibmacs/tpm2bios.log"
+            p2="$BASE_DIR/ibmacs/imasig.log"
+            # cmd 1&2 can add "-v" flag for verbose
+                #$BASE_DIR/ibmtss/utils/createekcert -rsa 2048 -cakey cakey.pem -capwd rrrr -vv
+                #$BASE_DIR/ibmtss/utils/createekcert -ecc nistp256 -cakey cakeyecc.pem -capwd rrrr -caalg ec -vv
+                #$BASE_DIR/ibmtss/utils/nvread -ha 01c00002 | awk 'NR==1 {next} {print}' | xxd -r -ps | base64 | sed -e '1i -----BEGIN CERTIFICATE-----' -e '$a -----END CERTIFICATE-----' > VMW_EK_CACERT.pem
+                #$BASE_DIR/ibmtss/utils/nvread -ha 01c0000a | awk 'NR==1 {next} {print}' | xxd -r -ps | base64 | sed -e '1i -----BEGIN CERTIFICATE-----' -e '$a -----END CERTIFICATE-----' > VMW_EKECC_CACERT.pem
+                #$BASE_DIR/ibmtss/utils/eventextend -if $p1 -tpm; \
+                #$BASE_DIR/ibmtss/utils/imaextend -if $p2 -le; \
+            sudo bash -c "\
+                cd $BASE_DIR/ibmacs; \
+                export LD_LIBRARY_PATH=\"$BASE_DIR/ibmtss/utils:$LD_LIBRARY_PATH\"; \
+                $BASE_DIR/ibmacs/client -alg ec -vv -ifb $p1 -ifi $p2 -ho $ACS_DEMO_SERVER_IP -ma $ACS_DEMO_CLIENT_IP"
+        elif [ $client_exec_approach -eq 2 ]; then
+            # this client_exec_approach directly calls setup_ibmtpm for consistantly remote attestation
+            #1> disable swtpm daemon
+            sudo systemctl stop swtpm.service
 
-        #2> display help info
-        echo_notice "$dirname" "$filename" "remember to restore swtpm daemon with following commands"
-        echo "sudo systemctl start swtpm.service"
-        echo "sleep 1"
-        echo "tpm2_startup -c"
-        cd ../setup_ibmtpm
-        #3> call setup_ibmtpm with interval var
-        echo_notice "$dirname" "$filename" "launching Remote Attestation script in setup_ibmtpm"
-        echo_notice "$dirname" "$filename" "choose ${RED}${BOLD}config_RA_client_launch.ini${END}"
-		lc1="echo \"launching Remote Attestation script in setup_ibmtpm\" && \
-			 echo -e \"choose \033[31m\033[1mconfig_RA_client_launch.ini\033[0m\""
-        lc2="sudo install_platform=5 user=$USER interval=$interval bash ./setup_sudo.sh || :"
-        newGterm "TPM SERVER" "$bash_gflag +e" "$lc1; $lc2" 1
+            #2> display help info
+            echo_notice "$dirname" "$filename" "remember to restore swtpm daemon with following commands"
+            echo "sudo systemctl start swtpm.service"
+            echo "sleep 1"
+            echo "tpm2_startup -c"
+            cd ../setup_ibmtpm
+            #3> call setup_ibmtpm with interval var
+            echo_notice "$dirname" "$filename" "launching Remote Attestation script in setup_ibmtpm"
+            echo_notice "$dirname" "$filename" "choose ${RED}${BOLD}config_RA_client_launch.ini${END}"
+            lc1="echo \"launching Remote Attestation script in setup_ibmtpm\" && \
+                 echo -e \"choose \033[31m\033[1mconfig_RA_client_launch.ini\033[0m\""
+            lc2="sudo install_platform=5 user=$USER interval=$interval bash ./setup_sudo.sh || :"
+            newGterm "TPM SERVER" "$bash_gflag +e" "$lc1; $lc2" 1
+        fi
+    fi
+    if [ $client_exec_task1 -eq 2 ]; then
+        echo_notice "$dirname" "$filename" "Launching traffic monitoring binary"
+        lc1="source $script_path/../common/functions.sh"
+        lc2="echo_notice \"$dirname\" \"$filename\" 'Traffic monitoring'"
+        lc3="sudo $script_path/traffic_AD/traffic_AD -H $TRAFFIC_MONITOR_HOST -i $TRAFFIC_MONITOR_INTERFACE -s 30 -c 3 -t 1000 -v || :"
+        #bash -c "$lc1; $lc2; $lc3"
+        newLXterm "Traffic Monitoring" "bash -c \"$lc1; $lc2; $lc3\"" 1
     fi
 }
 
